1d6f18725693837c1b5cd142175b01e44cc0274f
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-07 19:22:10 +0900

    UNSPLASH 랜덤이미지 인터페이스

---
+++ app/src/main/java/com/example/sampleimageloadapp/data/UnsplahApiService.kt
@@ -0,0 +1,22 @@
+package com.example.sampleimageloadapp.data
+
+import com.example.sampleimageloadapp.BuildConfig
+import com.example.sampleimageloadapp.data.model.PhotoResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UnsplahApiService {
+
+    @GET(
+        "photos/random?" +
+                "client_id = ${BuildConfig.UNSPLASH_ACCESS_KEY}"+
+                "&count=30"
+    )
+    suspend fun getRandomPhotos(
+        @Query("query") query : String?
+    ): Response<List<PhotoResponse>>
+
+
+
+}
