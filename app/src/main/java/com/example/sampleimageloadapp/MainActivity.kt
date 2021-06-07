afeb60b351114028f942c7d3732057684c566806
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-07 19:31:53 +0900

    랜덤 이미지 가져오기

--- app/src/main/java/com/example/sampleimageloadapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleimageloadapp/MainActivity.kt
@@ -2,12 +2,33 @@ package com.example.sampleimageloadapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sampleimageloadapp.data.Repository
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope = MainScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fetchRandomPhotos()
+    }
 
+    private fun fetchRandomPhotos(query : String? = null) =
+        scope.launch{
+            Repository.getRandomPhotos(query)?.let {photos ->
+
+                photos
+            }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
     }
 }
