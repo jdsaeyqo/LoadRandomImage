52a4337ee41ac71ccaed55b727e8ad475db29390
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-08 19:53:53 +0900

    LinksX 모델 코드수정

--- app/src/main/java/com/example/sampleimageloadapp/data/model/LinksX.kt
+++ app/src/main/java/com/example/sampleimageloadapp/data/model/LinksX.kt
@@ -4,14 +4,14 @@ package com.example.sampleimageloadapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class LinksX(
-    @SerializedName("html")
+
     val html: String?,
-    @SerializedName("likes")
+
     val likes: String?,
-    @SerializedName("photos")
+
     val photos: String?,
-    @SerializedName("portfolio")
+
     val portfolio: String?,
-    @SerializedName("self")
+
     val self: String?
 )
