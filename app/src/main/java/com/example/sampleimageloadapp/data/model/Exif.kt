992c5bf7dad5fe842f4e5171c9efc1816a99e485
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-08 19:51:17 +0900

    Exif모델 코드수정

--- app/src/main/java/com/example/sampleimageloadapp/data/model/Exif.kt
+++ app/src/main/java/com/example/sampleimageloadapp/data/model/Exif.kt
@@ -4,16 +4,16 @@ package com.example.sampleimageloadapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Exif(
-    @SerializedName("aperture")
+
     val aperture: String?,
-    @SerializedName("exposure_time")
+
     val exposureTime: String?,
-    @SerializedName("focal_length")
+
     val focalLength: String?,
-    @SerializedName("iso")
+
     val iso: Int?,
-    @SerializedName("make")
+
     val make: String?,
-    @SerializedName("model")
+
     val model: String?
 )
