a5062a0b4709ebba368c19150464a1aec8be89c7
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-08 19:54:27 +0900

    Location 모델 코드수정

--- app/src/main/java/com/example/sampleimageloadapp/data/model/Location.kt
+++ app/src/main/java/com/example/sampleimageloadapp/data/model/Location.kt
@@ -4,12 +4,8 @@ package com.example.sampleimageloadapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Location(
-    @SerializedName("city")
     val city: String?,
-    @SerializedName("country")
     val country: String?,
-    @SerializedName("name")
     val name: String?,
-    @SerializedName("position")
     val position: Position?
 )
