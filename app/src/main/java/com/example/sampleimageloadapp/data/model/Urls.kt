e18f1abd111363f0e4991fcc7b6ca59d981d4318
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-08 19:56:06 +0900

    Urls 모델 코드수정

--- app/src/main/java/com/example/sampleimageloadapp/data/model/Urls.kt
+++ app/src/main/java/com/example/sampleimageloadapp/data/model/Urls.kt
@@ -4,14 +4,9 @@ package com.example.sampleimageloadapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Urls(
-    @SerializedName("full")
     val full: String?,
-    @SerializedName("raw")
     val raw: String?,
-    @SerializedName("regular")
     val regular: String?,
-    @SerializedName("small")
     val small: String?,
-    @SerializedName("thumb")
     val thumb: String?
 )
