cce7f1e16a3883182e3930e65fe5f58ef11d8314
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-12 15:25:06 +0900

    사진 저장하기

--- app/src/main/java/com/example/sampleimageloadapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleimageloadapp/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.sampleimageloadapp
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.sampleimageloadapp.data.Repository
+import com.example.sampleimageloadapp.data.model.PhotoResponse
 import com.example.sampleimageloadapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
@@ -33,22 +45,22 @@ class MainActivity : AppCompatActivity() {
         fetchRandomPhotos()
     }
 
-    private fun initViews(){
+    private fun initViews() {
 
-        binding.recyclerView.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         binding.recyclerView.adapter = PhotoAdapter()
 
     }
 
     @SuppressLint("ServiceCast")
-    private fun bindViews(){
-        binding.searchEditText.setOnEditorActionListener { editText , actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_SEARCH){
-                currentFocus?.let {view ->
+    private fun bindViews() {
+        binding.searchEditText.setOnEditorActionListener { editText, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                currentFocus?.let { view ->
 
                     val inputMethodManager =
                         getSystemService(Context.INPUT_SERVICE) as? InputMethodManager
-                    inputMethodManager?.hideSoftInputFromWindow(view.windowToken,0)
+                    inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
 
                     view.clearFocus()
                 }
@@ -62,10 +74,15 @@ class MainActivity : AppCompatActivity() {
         binding.refreshLayout.setOnRefreshListener {
             fetchRandomPhotos(binding.searchEditText.text.toString())
         }
+
+        (binding.recyclerView.adapter as? PhotoAdapter)?.onClickPhoto = { photo ->
+
+            showDownloadPhotoConfirmationDialog(photo)
+        }
     }
 
-    private fun fetchRandomPhotos(query : String? = null) =
-        scope.launch{
+    private fun fetchRandomPhotos(query: String? = null) =
+        scope.launch {
             try {
                 Repository.getRandomPhotos(query)?.let { photos ->
 
@@ -79,19 +96,125 @@ class MainActivity : AppCompatActivity() {
 
                 }
                 binding.recyclerView.visibility = View.VISIBLE
-            }catch (exception : Exception){
+            } catch (exception: Exception) {
 
-                    binding.recyclerView.visibility = View.INVISIBLE
-                    binding.errorDescriptionTextVIew.visibility=View.VISIBLE
+                binding.recyclerView.visibility = View.INVISIBLE
+                binding.errorDescriptionTextVIew.visibility = View.VISIBLE
 
-                }finally {
+            } finally {
 
-                    binding.shimmerLayout.visibility = View.GONE
-                    binding.refreshLayout.isRefreshing = false
-                }
+                binding.shimmerLayout.visibility = View.GONE
+                binding.refreshLayout.isRefreshing = false
+            }
 
+
+        }
+
+    private fun showDownloadPhotoConfirmationDialog(photo: PhotoResponse) {
+
+        AlertDialog.Builder(this)
+            .setMessage("사진을 저장하시겠습니까")
+            .setPositiveButton("저장") { dialog, _ ->
+                downloadPhoto(photo.urls?.full)
+                dialog.dismiss()
 
             }
+            .setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+
+            }
+            .create()
+            .show()
+
+    }
+
+    private fun downloadPhoto(photoUrl: String?) {
+
+        photoUrl ?: return
+
+
+
+        Glide.with(this)
+            .asBitmap()
+            .load(photoUrl)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(
+                object : CustomTarget<Bitmap>(SIZE_ORIGINAL, SIZE_ORIGINAL) {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+
+                        saveBitmapToMediaStore(resource)
+
+                    }
+
+                    override fun onLoadStarted(placeholder: Drawable?) {
+                        super.onLoadStarted(placeholder)
+                        Snackbar.make(
+                            binding.root,
+                            "다운로드 중...",
+                            Snackbar.LENGTH_INDEFINITE
+                        ).show()
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) = Unit
+
+                    override fun onLoadFailed(errorDrawable: Drawable?) {
+                        super.onLoadFailed(errorDrawable)
+                        Snackbar.make(
+                            binding.root,
+                            "다운로드 실패",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+
+                }
+            )
+
+    }
+
+    private fun saveBitmapToMediaStore(bitmap: Bitmap){
+
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        val resolver = applicationContext.contentResolver
+        val imageCollectionUri =
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                MediaStore.Images.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL_PRIMARY
+                )
+
+            }else{
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            }
+
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME,fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE,"image/jpg")
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                put(MediaStore.Images.Media.IS_PENDING,1)
+            }
+        }
+
+        val imageUri = resolver.insert(imageCollectionUri,imageDetails)
+
+        imageUri ?: return
+
+        resolver.openOutputStream(imageUri).use {outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG,10,outputStream)
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            imageDetails.clear()
+            imageDetails.put(MediaStore.Images.Media.IS_PENDING,0)
+            resolver.update(imageUri,imageDetails,null,null)
+        }
+
+        Snackbar.make(binding.root,"다운로드 완료",Snackbar.LENGTH_SHORT).show()
+
+
+    }
 
     override fun onDestroy() {
         super.onDestroy()
