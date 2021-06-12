a743d30b8d4045ab34f60d6427d9d46367a0de7b
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-12 15:33:07 +0900

    sdk 버전 28이하 권한 설정

--- app/src/main/java/com/example/sampleimageloadapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleimageloadapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sampleimageloadapp
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Build
@@ -13,6 +15,7 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -42,7 +45,13 @@ class MainActivity : AppCompatActivity() {
 
         initViews()
         bindViews()
-        fetchRandomPhotos()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            fetchRandomPhotos()
+        } else {
+            requestWriteStoragePermission()
+        }
+
     }
 
     private fun initViews() {
@@ -174,46 +183,75 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun saveBitmapToMediaStore(bitmap: Bitmap){
+    private fun saveBitmapToMediaStore(bitmap: Bitmap) {
 
         val fileName = "${System.currentTimeMillis()}.jpg"
         val resolver = applicationContext.contentResolver
         val imageCollectionUri =
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 MediaStore.Images.Media.getContentUri(
                     MediaStore.VOLUME_EXTERNAL_PRIMARY
                 )
 
-            }else{
+            } else {
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI
             }
 
         val imageDetails = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME,fileName)
-            put(MediaStore.MediaColumns.MIME_TYPE,"image/jpg")
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-                put(MediaStore.Images.Media.IS_PENDING,1)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.Media.IS_PENDING, 1)
             }
         }
 
-        val imageUri = resolver.insert(imageCollectionUri,imageDetails)
+        val imageUri = resolver.insert(imageCollectionUri, imageDetails)
 
         imageUri ?: return
 
-        resolver.openOutputStream(imageUri).use {outputStream ->
-            bitmap.compress(Bitmap.CompressFormat.JPEG,10,outputStream)
+        resolver.openOutputStream(imageUri).use { outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 10, outputStream)
         }
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             imageDetails.clear()
-            imageDetails.put(MediaStore.Images.Media.IS_PENDING,0)
-            resolver.update(imageUri,imageDetails,null,null)
+            imageDetails.put(MediaStore.Images.Media.IS_PENDING, 0)
+            resolver.update(imageUri, imageDetails, null, null)
         }
 
-        Snackbar.make(binding.root,"다운로드 완료",Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(binding.root, "다운로드 완료", Snackbar.LENGTH_SHORT).show()
 
 
+    }
+
+    private fun requestWriteStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION
+
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val writeExternalStoragePermissionGranted =
+            requestCode == REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (writeExternalStoragePermissionGranted) {
+            fetchRandomPhotos()
+        }
+    }
+
+    companion object {
+        private const val REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 101
     }
 
     override fun onDestroy() {
