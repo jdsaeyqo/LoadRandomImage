8270fcf2e7d51b8017542160bc477aa781c0f459
jdsaeyqo <jdsaeyqi@naver.com>
2021-06-12 15:00:10 +0900

    클릭 리스너

--- app/src/main/java/com/example/sampleimageloadapp/PhotoAdapter.kt
+++ app/src/main/java/com/example/sampleimageloadapp/PhotoAdapter.kt
@@ -13,6 +13,8 @@ class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
 
     var photos: List<PhotoResponse> = emptyList()
 
+    var onClickPhoto : (PhotoResponse) -> Unit = {}
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
 
@@ -32,8 +34,15 @@ class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int = photos.size
 
-    class ViewHolder(private val binding: ItemPhotoBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class ViewHolder(private val binding: ItemPhotoBinding) : RecyclerView.ViewHolder(binding.root){
 
+        init {
+
+            binding.root.setOnClickListener {
+                onClickPhoto(photos[adapterPosition])
+            }
+
+        }
 
         fun bind(photo : PhotoResponse){
 
